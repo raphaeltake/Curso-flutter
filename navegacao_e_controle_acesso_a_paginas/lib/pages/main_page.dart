6770702bc468e8070aadc6c3705a098db99f27ff
raphaeltake <raph.takeuchi@gmail.com>
2026-02-20 13:25:45 -0300

    Drawer e Divider

--- navegacao_e_controle_acesso_a_paginas/lib/pages/main_page.dart
+++ navegacao_e_controle_acesso_a_paginas/lib/pages/main_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
@@ -10,8 +12,50 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Meu App"),),
-    );
+    return SafeArea(
+        child: Scaffold(
+          appBar: AppBar(title: Text("Main Page"),
+          ),
+          drawer: Drawer(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    width: double.infinity,
+                    child: InkWell(
+                      child: Text("Dados cadastrais"),
+                      onTap: (){},
+                    ),
+                  ),
+                  Divider(),
+                  // SizedBox(height: 10,),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    width: double.infinity,
+                    child: InkWell(
+                      child: Text("Configuraçṍes"),
+                      onTap: (){},
+                    ),
+                  ),
+                  Divider(),
+                  // SizedBox(height: 10,),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    width: double.infinity,
+                    child: InkWell(
+                      child: Text("Termos de uso e privacidade"),
+                      onTap: (){},
+                    ),
+                  ),
+                  Divider(),
+                ],
+              ),
+            ),
+          ),
+        )
+        );
   }
 }
